b922e1b8f0c5677e7efcb7cb1525dc3d7b909051
manuXD270516 <manueljss@live.com>
2022-07-23 14:37:50 -0400

    feat(DEV): Cambio de IP al server de produccion

--- app/src/main/java/com/example/tallerredes/VariablesGlobales.java
+++ app/src/main/java/com/example/tallerredes/VariablesGlobales.java
@@ -5,7 +5,7 @@ public class VariablesGlobales {
     public static String longitud;
     public static String tiempoConexion;
 
-    //public static final String BASE_URL = "http://54.232.23.102:3006/api/";
-    public static final String BASE_URL = "http://192.168.0.3:3005/api/";
+    public static final String BASE_URL = "http://54.232.23.102:3006/api/";
+    //public static final String BASE_URL = "http://192.168.0.3:3005/api/";
 
 }
